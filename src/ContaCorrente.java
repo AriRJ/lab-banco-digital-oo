0e828a1a7516510b1509315be999387573692ecb
Ari Alberto <154787080+AriRJ@users.noreply.github.com>
2024-06-13 04:25:25 -0300

    Update ContaCorrente.java

--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -1,14 +1,13 @@
 
 public class ContaCorrente extends Conta {
+    private static final double TAXA_OPERACAO = 0.20;
 
-	public ContaCorrente(Cliente cliente) {
-		super(cliente);
-	}
+    public ContaCorrente(Cliente cliente) {
+        super(cliente);
+    }
 
-	@Override
-	public void imprimirExtrato() {
-		System.out.println("=== Extrato Conta Corrente ===");
-		super.imprimirInfosComuns();
-	}
-	
-}
+    @Override
+    public void sacar(double valor) {
+        super.sacar(valor + TAXA_OPERACAO);
+        historicoTransacoes.add(new Transacao("Taxa de Operação
+
