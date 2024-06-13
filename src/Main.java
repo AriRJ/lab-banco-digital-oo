dee56e51a755560513241a5df5c06c0dd786155c
Ari Alberto <154787080+AriRJ@users.noreply.github.com>
2024-06-13 04:27:00 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,21 @@
 
 public class Main {
+    public static void main(String[] args) {
+        Cliente venilton = new Cliente();
+        venilton.setNome("Venilton");
 
-	public static void main(String[] args) {
-		Cliente venilton = new Cliente();
-		venilton.setNome("Venilton");
-		
-		Conta cc = new ContaCorrente(venilton);
-		Conta poupanca = new ContaPoupanca(venilton);
+        Conta cc = new ContaCorrente(venilton);
+        Conta poupanca = new ContaPoupanca(venilton);
 
-		cc.depositar(100);
-		cc.transferir(100, poupanca);
-		
-		cc.imprimirExtrato();
-		poupanca.imprimirExtrato();
-	}
+        cc.depositar(100);
+        cc.transferir(50, poupanca);
 
+        cc.imprimirExtrato();
+        poupanca.imprimirExtrato();
+
+        // Simulando o rendimento anual na conta poupança
+        ((ContaPoupanca) poupanca).aplicarRendimento();
+        poupanca.imprimirExtrato();
+    }
 }
+
