7f658de45af26eae94e7a91c4838315269ca30e0
Ari Alberto <154787080+AriRJ@users.noreply.github.com>
2024-06-13 04:26:16 -0300

    Update ContaPoupanca.java

--- src/ContaPoupanca.java
+++ src/ContaPoupanca.java
@@ -1,13 +1,21 @@
 
 public class ContaPoupanca extends Conta {
+    private static final double RENDIMENTO_ANUAL = 0.05;
 
-	public ContaPoupanca(Cliente cliente) {
-		super(cliente);
-	}
+    public ContaPoupanca(Cliente cliente) {
+        super(cliente);
+    }
 
-	@Override
-	public void imprimirExtrato() {
-		System.out.println("=== Extrato Conta Poupança ===");
-		super.imprimirInfosComuns();
-	}
+    public void aplicarRendimento() {
+        double rendimento = saldo * RENDIMENTO_ANUAL;
+        saldo += rendimento;
+        historicoTransacoes.add(new Transacao("Rendimento Anual", rendimento));
+    }
+
+    @Override
+    public void imprimirExtrato() {
+        System.out.println("=== Extrato Conta PoupanÃ§a ===");
+        super.imprimirInfosComuns();
+        super.imprimirHistoricoTransacoes();
+    }
 }
